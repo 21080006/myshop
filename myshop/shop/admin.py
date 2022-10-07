e20271b838af8d14cb0dbfaa1d1d6511abae60b0
21080006 <21080006@upemor.edu.mx>
2022-10-06 21:08:01 -0500

    agregando el proyecto final version 2

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Category, Product
-# Register your models here.
-from .models import Category, Product
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,9 +8,9 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
-
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price','available', 'created', 'updated']
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .models import Category, Product
 
-# Create your views here.
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -11,14 +11,20 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
-    'shop/product/list.html',
-    {'category': category,
-    'categories': categories,
-    'products': products})
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
     cart_product_form = CartAddProductForm()
+
     return render(request,
-    'shop/product/detail.html',
-    {'product': product,'cart_product_form': cart_product_form})
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
